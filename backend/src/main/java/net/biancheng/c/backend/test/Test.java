e5b298cee0c9179899f17bca33f5e0aa9d672bae
wdrdssd <2786888512@qq.com>
2025-08-21 18:57:09 +0800

    revision:first

--- backend/src/main/java/net/biancheng/c/backend/test/Test.java
+++ backend/src/main/java/net/biancheng/c/backend/test/Test.java
@@ -2,6 +2,6 @@ package net.biancheng.c.backend.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello");
     }
 }
